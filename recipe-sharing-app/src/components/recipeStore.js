8a575a57a039ba28d58911f92951f68d020cd441
VampyrFangs <testezmonclic@gmail.com>
2024-12-15 23:33:40 +0100

    ....

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -6,4 +6,11 @@ const useRecipeStore = create(set => ({
   setRecipes: (recipes) => set({ recipes }),
   deleteRecipe: (deleteRecipe) => set(state => ({ recipes: [...state.recipes, deleteRecipe]})),
   updateRecipe: (updateRecipe) => set(state => ({ recipes: [...state.recipes, updateRecipe]})),
+  searchTerm: '',
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filteredRecipes: [],
+  filterRecipes: () => set(state => ({
+    filteredRecipes: state.recipes.filter(recipe =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase()))
+    })),
 }));
