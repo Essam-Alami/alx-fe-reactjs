56fc1c5aba840fd7508a60a6c73d50e5bb400217
VampyrFangs <testezmonclic@gmail.com>
2024-12-15 22:44:31 +0100

    ...

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,5 +3,7 @@ import create from 'zustand'
 const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+  setRecipes: (recipes) => set({ recipes }),
+  deleteRecipe: (deleteRecipe) => set(state => ({ recipes: [...state.recipes, deleteRecipe]})),
+  updateRecipe: (updateRecipe) => set(state => ({ recipes: [...state.recipes, updateRecipe]})),
 }));
