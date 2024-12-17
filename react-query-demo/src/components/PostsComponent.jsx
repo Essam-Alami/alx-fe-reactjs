8de73ebc7707da512d2993b60b35e8346e2d35c9
VampyrFangs <testezmonclic@gmail.com>
2024-12-17 19:33:04 +0100

    ....

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -6,7 +6,7 @@ const fetchPosts = async () => {
 };
 
 const PostsComponent = () => {
-  const { data, error, isLoading } = useQuery('posts', fetchPosts);
+  const { data, isError, isLoading } = useQuery('posts', fetchPosts);
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error loading posts</div>;
