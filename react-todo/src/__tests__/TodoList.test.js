6a537453ce93d959ed9b0de447e6e1e2086df567
VampyrFangs <testezmonclic@gmail.com>
2024-12-18 00:50:40 +0100

    ....

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import TodoList from '../TodoList.jsx';
+import TodoList from '../TodoList';
 
 test('renders initial todos', () => {
   render(<TodoList />);
@@ -9,22 +8,3 @@ test('renders initial todos', () => {
   expect(screen.getByText('Build a Todo App')).toBeInTheDocument();
 });
 
-test('adds a new todo', () => {
-  render(<TodoList />);
-  fireEvent.change(screen.getByPlaceholderText('Add a new todo'), { target: { value: 'New Todo' } });
-  fireEvent.click(screen.getByText('Add'));
-  expect(screen.getByText('New Todo')).toBeInTheDocument();
-});
-
-test('toggles a todo', () => {
-  render(<TodoList />);
-  fireEvent.click(screen.getByText('Learn React'));
-  expect(screen.getByText('Learn React')).toHaveStyle('text-decoration: line-through');
-});
-
-test('deletes a todo', () => {
-  render(<TodoList />);
-  const deleteButtons = screen.getAllByText('Delete');
-  fireEvent.click(deleteButtons[0]); // Delete the first todo
-  expect(screen.queryByText('Learn React')).not.toBeInTheDocument();
-});
