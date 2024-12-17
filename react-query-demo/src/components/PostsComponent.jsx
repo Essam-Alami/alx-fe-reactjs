7894ad537570caff5f490dfa840325dda46c6ef8
VampyrFangs <testezmonclic@gmail.com>
2024-12-17 19:31:21 +0100

    ....

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,26 @@
+import { useQuery } from 'react-query';
+
+const fetchPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  return response.json();
+};
+
+const PostsComponent = () => {
+  const { data, error, isLoading } = useQuery('posts', fetchPosts);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading posts</div>;
+
+  return (
+    <div>
+      {data.map(post => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostsComponent;
