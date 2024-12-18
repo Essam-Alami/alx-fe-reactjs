de762463d9f423e9763107e2f6a8f1ac0bc5828e
VampyrFangs <testezmonclic@gmail.com>
2024-12-18 14:45:31 +0100

    .

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import RecipeDetail from './components/RecipeDetail';
-import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
   return (
