8a9ebaa6366df2243166c36100cc3f1c477ac546
VampyrFangs <testezmonclic@gmail.com>
2024-12-18 15:10:32 +0100

    .

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -17,7 +17,7 @@ const Search = () => {
       const data = await fetchAdvancedUserData(username, location, minRepos);
       setUsers(data.items);
     } catch (err) {
-      setError('Looks like we can\'t find the user');
+      setError('Looks like we cant find the user');
     } finally {
       setLoading(false);
     }
