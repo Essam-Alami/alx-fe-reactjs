c89e6510ac284904723b996d414aac41f73b4e5f
VampyrFangs <testezmonclic@gmail.com>
2024-12-18 14:41:47 +0100

    .

---
+++ github-user-search/src/services/githubservice.js
@@ -0,0 +1,20 @@
+// src/services/githubService.js
+import axios from 'axios';
+
+const GITHUB_API_URL = 'https://api.github.com';
+
+const fetchUserData = async (username) => {
+  try {
+    const response = await axios.get(`${GITHUB_API_URL}/users/${username}`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_API_KEY}`
+      }
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    throw error;
+  }
+};
+
+export { fetchUserData };
